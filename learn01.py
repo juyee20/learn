283fdd9ea103f9dec8c0f3f3f149b9a45959964c
juyee20 <juyee20@163.com>
2019-12-30 14:21:26 +0800

    python

---
+++ learn01.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+
+counter = 100
+miles = 1000.00
+name = "hansyinfo"
+print(counter)
+print(miles)
+print(name)
+
+a = b = c = 1
+d,e,f = 5,6,'ok'
+
+if a > b :
+ print('Hello,中国!',end="")
+ print("OK")
+else:
+ print("test")
+
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(e)
+# print(f)
+# print(name[1:4:2] * 2)
+
+# testdict = { 'name':"hansy",'code':6734,'dept':'info'}
+# print(testdict)
+# print(testdict['code'])
+# print(testdict.keys())
+# print(testdict.values())
+print(a+d)
+print(a-d)
+print(a*d)
+print(e/d)
+print(e%4)
+print(d**2)
+print(d//2)
